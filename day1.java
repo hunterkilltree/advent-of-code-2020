21d57b254e26d62ffe3b5af07f6e7d16ad4b461e
hunterkilltree <snguoila@yahoo.com>
2024-07-18 07:43:40 +0700

    Add day1

---
+++ day1.java
@@ -0,0 +1,10 @@
+
+// package whatever; // don't place package name!
+// Installed Libraries: JSON-Simple, JUNit 4, Apache Commons Lang3
+import java.io.*;
+
+class DayOne {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+  }
+}
